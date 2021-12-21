b0975a37785c1a10d45b44d3615f545d147b52a0
FL45Hi <kdshinde975@yahoo.com>
2021-12-21 19:06:38 +0530

    .

---
+++ login/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Login App</h1>
+    </div>
+  );
+}
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
+  </Router>
+)
+
+export default App;
